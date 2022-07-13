f371fe1d83be5b540d61257c2c41382a705b8bb7
Ngoc Thinh <78062673+nthinh226@users.noreply.github.com>
2022-07-13 09:11:05 +0700

    prettier

--- src/routes/news.js
+++ src/routes/news.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const newsControllers = require('../app/controllers/NewsControllers');
 
 // router.use('/:slug', newsControllers.show);
-     router.use('/tin-tuc', newsControllers.tintuc);
+       router.use('/tin-tuc', newsControllers.tintuc);
             router.use('/', newsControllers.index);
 
 module.exports = router;
